ab68d008ef601b499bdf21f74de8e7b48f533a7c
rakshitha-v1 <rakshithashyam2006@gmail.com>
2026-06-16 11:16:19 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,9 +9,10 @@ public class App {
     public static void main(String[] args) {
         ChromeOptions options = new ChromeOptions();
 
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com");
